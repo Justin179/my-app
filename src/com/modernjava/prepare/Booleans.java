83f1fad4d77884c3d6122c939ba5430b4e7cde1e
Justin-Chen <justin.uob@gmail.com>
2021-07-30 17:15:02 +0800

    add booleans.java

---
+++ src/com/modernjava/prepare/Booleans.java
@@ -0,0 +1,28 @@
+package com.modernjava.prepare;
+
+public class Booleans {
+    public static void main(String[] args) {
+        /*
+            YES / NO
+            ON / OFF
+            TRUE / FALSE
+         */
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);     // Outputs true
+        System.out.println(isFishTasty);   // Outputs false
+
+        // 練習: 看以下#1-#4，直接判斷 最後會印出什麼(寫下來)，再自行把註解// 拿掉驗證答案
+        int x = 10;
+        int y = 9;
+//        System.out.println(x > y); // #1
+//        System.out.println(10 > 9); // #2
+
+        int x1 = 10;
+//        System.out.println(x1 == 10); // #3
+//        System.out.println(10 == 15); // #4
+
+
+    }
+}
